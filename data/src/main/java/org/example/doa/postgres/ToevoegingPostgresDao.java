c401134085e4778489bd514179db5d46f7eb34fa
Bart Rijnders <b.rijnders@student.fontys.nl>
2021-05-28 14:20:11 +0200

    postgres database persistie aangemaakt voor Toevoegingen

--- data/src/main/java/org/example/doa/postgres/ToevoegingPostgresDao.java
+++ data/src/main/java/org/example/doa/postgres/ToevoegingPostgresDao.java
@@ -12,7 +12,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -31,7 +30,7 @@ public class ToevoegingPostgresDao implements ToevoegingDao {
     @Override
     public List<Toevoeging> getAllToevoegingen(Maaltijd maaltijd) {
 
-        String sql = "SELECT * FROM toevoeging WHERE maaltijdid::text = ?";
+        String sql = "SELECT * FROM toevoeging WHERE maaltijdid::text = ?;";
         List<Toevoeging> toevoegingen  = new ArrayList<>();
         try(Connection conn = dBconnection.connect();
             PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
@@ -53,21 +52,66 @@ public class ToevoegingPostgresDao implements ToevoegingDao {
             throwables.printStackTrace();
         }
 
-        return null;
+        return toevoegingen;
     }
 
     @Override
     public void storeToevoeging(Toevoeging toevoeging, Maaltijd maaltijd) {
+        String sql = "INSERT INTO toevoeging (id, hoeveelheidingram, maaltijdid, itemid) " +
+                "VALUES (?,?,?,?);";
 
+        try(Connection conn = dBconnection.connect();
+            PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+
+            preparedStatement.setObject(1, toevoeging.getID());
+            preparedStatement.setDouble(2, toevoeging.getHoeveelheidInGram());
+            preparedStatement.setObject(3, maaltijd.getId());
+            preparedStatement.setObject(4,toevoeging.getItem().getID());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     @Override
     public void deleteToevoeging(Toevoeging toevoeging, Maaltijd maaltijd) {
+        String sql = "DELETE FROM toevoeging WHERE id::text = ?";
 
+        try(Connection conn = dBconnection.connect();
+            PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+
+            preparedStatement.setObject(1, toevoeging.getID());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     @Override
     public Toevoeging getByID(UUID id, Maaltijd maaltijd) {
-        return null;
+        String sql = "SELECT * FROM toevoeging WHERE id::text = ?;";
+        Toevoeging toevoeging = null;
+        try(Connection conn = dBconnection.connect();
+            PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+
+            preparedStatement.setString(1, maaltijd.getId().toString());
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                UUID uuid = UUID.fromString(resultSet.getString("id"));
+                int hig = resultSet.getInt("hoeveelheidingram");
+                UUID itemID = UUID.fromString(resultSet.getString("itemid"));
+                Item item = itemDoa.getItemByID(itemID);
+                toevoeging = new MaaltijdToevoeging(hig, item, uuid);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return toevoeging;
     }
 }
